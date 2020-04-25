b50a1cd32e8dc37e7043899412f55d81712f09f8
Saada2016 <saad.alikhan@cee-solutions.com>
2020-04-25 13:17:50 -0800

    Updates

---
+++ precache-manifest.5e468da29986f9bf942849f89352a260.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef477a1f37b640f75f37b48f2e53b50",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8a3e6ab80939738ba614",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e46517f5ddd258ec4841",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a3e6ab80939738ba614",
+    "url": "/portfolio/static/js/2.858d60fa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.858d60fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46517f5ddd258ec4841",
+    "url": "/portfolio/static/js/main.d7e9bfbc.chunk.js"
+  },
+  {
+    "revision": "66fe1e2f1fffedad1d76",
+    "url": "/portfolio/static/js/runtime-main.67b19624.js"
+  },
+  {
+    "revision": "345511f9b6955202fbf71d078e81c11a",
+    "url": "/portfolio/static/media/pic1.345511f9.jpg"
+  }
+]);
